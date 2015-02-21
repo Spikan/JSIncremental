2a9c6f345d86ebd2359642e913a5cfb3c6b167f9
Spikan <spikan@gmail.com>
2015-02-20 19:06:47 -0500

    lots of stuff

--- js/main.js
+++ js/main.js
@@ -5,46 +5,66 @@ var sps = 0;
 var autosave = "on";
 var autosaveCounter = 0;
 
-function sodaClick(number){
+regSoda = new Image();
+regSoda.src = "images/regSoda.png";
+moSoda = new Image();
+moSoda.src = "images/moSoda.png";
+clickSoda = new Image();
+clickSoda.src = "images/clickSoda.png";
+
+
+function sodaClick(number) {
     sips = sips + number;
-    document.getElementById("sips").innerHTML = sips;
+    document.getElementById("sips").innerHTML = prettify(sips);
+
+    setTimeout(function(){
+        document.sodaButton.src = regSoda.src;
+    }, 120);
+    document.sodaButton.src = clickSoda.src;
+};
+
+function spsClick(number) {
+    sips = sips + number;
+    document.getElementById("sips").innerHTML = prettify(sips);
+
 };
 
 
-
-function buyStraw(){
-    var strawCost = Math.floor(10 * Math.pow(1.1,straws));
-    if(sips >= strawCost){
+function buyStraw() {
+    var strawCost = Math.floor(10 * Math.pow(1.1, straws));
+    if (sips >= strawCost) {
         straws = straws + 1;
         sips = sips - strawCost;
         document.getElementById('straws').innerHTML = straws;
-        document.getElementById('sips').innerHTML = sips;
+        document.getElementById('sips').innerHTML = prettify(sips);
 
-        sps = sps + 1;
-        document.getElementById('sps').innerHTML = sps;
-    };
-    var nextStrawCost = Math.floor(10 * Math.pow(1.1,straws));
+        sps = sps + .4;
+        document.getElementById('sps').innerHTML = prettify(sps);
+    }
+    ;
+    var nextStrawCost = Math.floor(10 * Math.pow(1.1, straws));
     document.getElementById('strawCost').innerHTML = nextStrawCost;
 };
 
-function buyCup(){
-    var cupCost = Math.floor(20 * Math.pow(1.2,cups));
-    if(sips >= cupCost){
+function buyCup() {
+    var cupCost = Math.floor(20 * Math.pow(1.2, cups));
+    if (sips >= cupCost) {
         cups = cups + 1;
         sips = sips - cupCost;
         document.getElementById('cups').innerHTML = cups;
-        document.getElementById('sips').innerHTML = sips;
+        document.getElementById('sips').innerHTML = prettify(sips);
 
-        sps = sps + 2;
-        document.getElementById('sps').innerHTML = sps;
-    };
-    var nextCupCost = Math.floor(20 * Math.pow(1.2,cups));
+        sps = sps + 1;
+        document.getElementById('sps').innerHTML = prettify(sps);
+    }
+    ;
+    var nextCupCost = Math.floor(20 * Math.pow(1.2, cups));
     document.getElementById('cupCost').innerHTML = nextCupCost;
 
 
 };
 
-function save(){
+function save() {
 
     var save = {
         sips: sips,
@@ -53,10 +73,10 @@ function save(){
         sps: sps
     }
 
-    localStorage.setItem("save",JSON.stringify(save));
+    localStorage.setItem("save", JSON.stringify(save));
 }
 
-window.onload = function load(){
+window.onload = function load() {
 
 
     var savegame = JSON.parse(localStorage.getItem("save"));
@@ -66,8 +86,8 @@ window.onload = function load(){
     if (typeof savegame.cups !== "undefined") cups = savegame.cups;
     if (typeof savegame.sps !== "undefined") sps = savegame.sps;
 
-    var strawCost = Math.floor(10 * Math.pow(1.1,straws));
-    var cupCost = Math.floor(20 * Math.pow(1.2,cups));
+    var strawCost = Math.floor(10 * Math.pow(1.1, straws));
+    var cupCost = Math.floor(20 * Math.pow(1.2, cups));
 
 
     document.getElementById('straws').innerHTML = straws;
@@ -79,30 +99,28 @@ window.onload = function load(){
 
 }
 
-function delete_save(){
+function delete_save() {
     localStorage.removeItem("save")
 }
 
 
+function prettify(input){
+    var output = Math.round(input * 1000000)/1000000;
+    return output;
+}
 
-window.setInterval(function(){
+window.setInterval(function () {
 
-    sodaClick(sps);
+    spsClick(sps);
 
 
-    if(autosave == "on") {
-        autosaveCounter+=1;
-        if(autosaveCounter>=60)
-        {
+    if (autosave == "on") {
+        autosaveCounter += 1;
+        if (autosaveCounter >= 60) {
             save();
-            autosaveCounter=1;
+            autosaveCounter = 1;
         }
     }
 
 
 }, 1000);
-
-window.setInterval(function(){
-
-
-}, 2000);
