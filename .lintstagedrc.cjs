c383b6beddb7f7006e1b6bbf50b2e2c6224c9755
The Grickus <spikan@gmail.com>
2026-03-12 08:07:56 -0400

    chore: tighten repo hygiene

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
   '*.{js,ts}': ['eslint --fix', 'prettier --write'],
   '*.{json,md,html,css}': ['prettier --write'],
-  '*.{ts}': ['tsc --noEmit'],
 };
--- .lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-export default {
-  '*.{js,ts}': ['eslint --fix', 'prettier --write'],
-  '*.{json,md,html,css}': ['prettier --write'],
-  '*.{ts}': ['tsc --noEmit'],
-};
